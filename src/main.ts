4bcc0d5be912195ec46cb383795f611131497f74
jonz94 <jody16888@gmail.com>
2023-01-31 20:32:07 +0800

    feat: setup `@ngrx/router-store` using standalone api

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app/app.component';
@@ -11,6 +12,10 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideStore(),
+    provideState({
+      router: routerReducer,
+    }),
+    provideRouterStore(),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
